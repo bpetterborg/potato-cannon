a94dee51f6047fb9723b967cacae664da268fa47
bpetterborg <68480079+bpetterborg@users.noreply.github.com>
2020-10-14 17:31:29 -0400

    Updated stuff

---
+++ buttonControl.py
@@ -0,0 +1,27 @@
+# controls a potato cannon with a Raspbery Pi
+
+# to-do
+# make it so I don't have to ssh into it or use a button for firing
+
+
+# importing stuff
+import gpiozero
+from time import sleep
+
+# variables
+relay = LED(17)
+button = Button(2)
+timeOpen = 0.7 # time to open relay for
+
+# activate relay for 
+while True:
+    
+    if button.when_released:
+        
+        print('firing... \n')
+        relay.on()
+        sleep(timeOpen)
+        relay.off()
+    
+    else:
+        relay.off()
